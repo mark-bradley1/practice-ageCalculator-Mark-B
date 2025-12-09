95c98aed60a8b28a701b437e12d658c8d4a64be7
mark-bradley1 <mbradle15@gmail.com>
2025-12-08 20:06:52 -0600

    completed assignment

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -1,7 +1,18 @@
 package org.example;
+import java.util.Scanner;
 
 public class Main {
     public static void main(String[] args) {
-d
+        Scanner input = new Scanner(System.in);
+
+        System.out.print("Please enter your age: ");
+
+        int age = input.nextInt();
+
+        int yearsLeft = 100 - age;
+
+        System.out.println("You have " + yearsLeft + " years until you are 100 years old.");
+
+        input.close();
     }
 }
